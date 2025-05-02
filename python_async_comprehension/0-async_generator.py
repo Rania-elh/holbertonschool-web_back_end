d74f0943625e2a6dbba14dff22ca817f29a1cedd
Rania-elh <rania1elhefnawy@gmail.com>
2025-05-02 17:24:19 +0200

    Async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -13,5 +13,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.randint(0, 10)
 
