00203bc2bf5029c45f4400612c67d6010c474809
Rania-elh <rania1elhefnawy@gmail.com>
2025-05-21 19:12:24 +0200

    Update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
-const std = require('process');
+// Gets input from user and writes it to stdout
+process.stdin.setEncoding('utf-8'); // set encoding to utf-8
 
-std.stdout.write('Welcome to Holberton School, what is your name?\n');
-std.stdin.on('readable', () => {
-  const name = std.stdin.read();
-  if (name) {
-    std.stdout.write(`Your name is: ${name}`);
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read(); // read input from stdin
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-std.stdin.on('end', () => {
-  console.log('This important software is now closing');
+/**
+ * If stdout process in stream comes from the terminal, end the process
+ * with a message
+ */
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
